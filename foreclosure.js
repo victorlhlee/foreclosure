11281225cb246ef12136e078bb41b0ff5798061c
Victor <viclee02@gmail.com>
2016-06-08 22:18:34 -1000

    refactor exercises

--- foreclosure.js
+++ foreclosure.js
@@ -1,5 +1,86 @@
 //strict mode
+"use strict";
+
 var steve;
+var stevesLoan;
+var month = 0;
+var monthsUntilEvicted;
+
+function loan(){
+  var account = {
+    borrowed: 550000,
+    balance: 286000,
+    monthlyPayment: 1700,
+    defaulted: 0,
+    defaultsToForeclose: 5,
+    foreclosed: false
+  };
+}
+
+function missPayment(){
+  account.defaulted++;
+  if(account.defaulted >= account.defaultsToForeclose){
+    account.foreclosed = true;
+  }
+}
+
+return {
+  getBalance: function(){
+    return account.balance;
+  },
+  receivePayment: function(amount){
+    if(amount < account.monthlyPayment){
+      missPayment();
+    }else{
+      account.balance -= amount;
+    }
+  },
+  getMonthlyPayment: function(){
+    return account.monthlyPayment;
+  },
+  isForeclosed: function(){
+    return account.foreclosed;
+  },
+};
+
+function borrower(loan){
+  var account = {
+    monthlyIncome: 1350,
+    funds: 2800,
+    loan: loan
+  };
+  return {
+    getFunds: function(){
+      return account.funds;
+    },
+    makePayment: function(){
+      if(account.funds > account.monthlyPayment){
+        account.funds -= account.monthlyPayment;
+        receivePayment(account.monthlyPayment);
+      }else{
+        receivePayment(account.funds);
+        account.funds = 0;
+      }
+    },
+    payDay: function(){
+      account.funds += account.monthlyIncome;
+    }
+  }
+};//borrower function
+
+
+stevesLoan = loan();
+steve = borrow(stevesLoan);
+
+while(!stevesLoan.isForeclosed()){
+  steve.payday();
+  steve.makePayment();
+  month++;
+}
+monthsUntilEvicted = month;
+
+
+/*var steve;
 var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
@@ -76,4 +157,4 @@ while(!stevesLoan.isForeclosed()){
   month++;
 }
 
-monthsUntilEvicted = month;
+monthsUntilEvicted = month;*/
